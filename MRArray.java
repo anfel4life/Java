f5c4326f8e776ec5aced9d9212268b6815ed5fe8
OVakhnenko <oleksiy@vakhnenko.com>
2017-01-18 17:42:00 +0300

    fix

--- MRArray.java
+++ MRArray.java
@@ -1,67 +1,75 @@
+package Mentor;
+
 import java.io.*;
 import java.util.*;
 
 public class MRArray {
-	private static int TMP[] = {7,3,9,2,5,1,6,0,6,3};
-	private static int arr[] = new int[10]; 
-	private File txt;
-	private String fileName;
-	
-	MRArray() {
-	}
-	void sortLoToHi() {
-		for(int i = arr.length-1; i > 0; i--) {
-			for(int j = 0; j < i; j++) {
-				if( arr[j] > arr[j + 1] ){
-	                int tmp = arr[j];
-	                arr[j] = arr[j + 1];
-	                arr[j + 1] = tmp;
-	            }
-			}
-		}
-	}
-	void createFile() {
-		fileName = new File("").getAbsolutePath() + "\\MRArray.txt";
-		txt = new File(fileName);
-		try {
-			txt.createNewFile();
-		} catch (IOException  e) {
-			System.out.println("Error 1");			
-		}
-	}
-	void writeArrToFile(int[] tmpArr) {
-		try (FileWriter fw = new FileWriter(fileName)) {
-			for (int i = 0; i < tmpArr.length; i++) {
-				fw.write(tmpArr[i] + '0');
-				fw.flush();
-			}
-			fw.close();
-		} catch  (IOException  e) {
-			System.out.println("Error 2");
-		}
-		
-	}
-	void readArrFromFile() {
-	    try {
-			Scanner sc = new Scanner(txt);
-			sc.useDelimiter("");
-			for (int i = 0; i < 10; i++) arr[i] = sc.nextInt();
-		} catch (Exception e) {
-			System.out.println("Error 3");
-		}
-	}
-	void print() {
-		for(int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-	public static void main(String[] args) {
-		MRArray mra = new MRArray();
-		mra.createFile();
-		mra.writeArrToFile(TMP);
-		mra.readArrFromFile();
-		mra.print();
-		mra.sortLoToHi();
-		mra.writeArrToFile(arr);
-		mra.print();
-	}
+    private static int TMP[] = {7, 3, 9, 2, 5, 1, 6, 0, 6, 3};
+    private static int arr[] = new int[10];
+    private File txt;
+    private String fileName;
+
+    MRArray() {
+    }
+
+    void sortLoToHi() {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+    void createFile() {
+        fileName = new File("").getAbsolutePath() + "\\MRArray.txt";
+        txt = new File(fileName);
+        try {
+            txt.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error 1");
+        }
+    }
+
+    void writeArrToFile(int[] tmpArr) {
+        try (FileWriter fw = new FileWriter(fileName)) {
+            for (int i = 0; i < tmpArr.length; i++) {
+                fw.write(tmpArr[i] + '0');
+                fw.flush();
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error 2");
+        }
+
+    }
+
+    void readArrFromFile() {
+        try {
+            Scanner sc = new Scanner(txt);
+            sc.useDelimiter("");
+            for (int i = 0; i < 10; i++) arr[i] = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error 3");
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MRArray mra = new MRArray();
+        mra.createFile();
+        mra.writeArrToFile(TMP);
+        mra.readArrFromFile();
+        mra.print();
+        mra.sortLoToHi();
+        mra.writeArrToFile(arr);
+        mra.print();
+    }
 }
