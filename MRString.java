bcf7bc047918cd6565be37ab2382a2148112b540
OVakhnenko <oleksiy@vakhnenko.com>
2017-01-19 14:05:11 +0300

    ...

--- MRString.java
+++ MRString.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 public class MRString {
+    String createString() {
+        int len;
+        String str = "";
+
+        Random rnd = new Random();
+        len = rnd.nextInt(1000);
+        for (int i = 0; i < len; i++) {
+            str += (char) rnd.nextInt(256);
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
+        MRString mrs = new MRString();
+        System.out.println(mrs.createString());
+        System.out.println(mrs.createString());
     }
 }
