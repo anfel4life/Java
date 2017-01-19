4ffc0ada773abcba56fd7c7b0c7ce09025a1c067
OVakhnenko <oleksiy@vakhnenko.com>
2017-01-19 13:26:25 +0300

    ...

---
+++ MRHashCode.java
@@ -0,0 +1,8 @@
+public class MRHashCode {
+    public static void main(String[] args) {
+        String st1 = "123456788";
+        System.out.println(st1.hashCode());
+        String st2 = "123456788";
+        System.out.println(st2.hashCode());
+    }
+}
